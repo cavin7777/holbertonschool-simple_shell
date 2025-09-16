87dedec7d923ce8136f93cde420d671c83a89441
cavin7777 <caven7777@gmail.com>
2025-09-16 13:20:59 +0400

    task_2_correc7

--- simple_shell.c
+++ simple_shell.c
@@ -4,6 +4,10 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <string.h>
+#include <errno.h>
+
+extern char **environ;
 
 int main(void)
 {
@@ -12,48 +16,46 @@ int main(void)
 	ssize_t read;
 	pid_t pid;
 	int status;
-	int interactive = isatty(STDIN_FILENO);
 
 	while (1)
 	{
-		if (interactive)
-		{
-			printf("$simple_shell$ ");
-			fflush(stdout);
-		}
+
+		printf("$simple_shell$ ");
+		fflush(stdout);
 
 		read = getline(&line, &len, stdin);
 		if (read == -1)
 		{
-			if (interactive)
-				printf("\n");
+			printf("\n");
 			break;
 		}
 
 		if (line[read - 1] == '\n')
 			line[read - 1] = '\0';
 
-		if (*line == '\0')
+		if (line[0] == '\0')
 			continue;
 
 		pid = fork();
 		if (pid == -1)
 		{
-			perror("./shell");
-			free(line);
-			return (1);
+			perror("fork");
+			continue;
 		}
 
 		if (pid == 0)
 		{
-			char *argv[2] = {line, NULL};
+			char *argv[2];
 
-			if (execvp(line, argv) == -1)
+			argv[0] = line;
+			argv[1] = NULL;
+
+			if (execve(line, argv, environ) == -1)
 			{
 				perror("./shell");
 				exit(EXIT_FAILURE);
-
 			}
+
 		}
 		else
 		{
